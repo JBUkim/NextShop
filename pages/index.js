684f3ac50e57f768e1a60a3d766107afccb93150
JBUkim <91812103@jmail.ac.kr>
2022-10-14 10:06:41 +0900

    8-SaveCartItems

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
 import data from '../utils/data'
 import ProductItem from '../components/Productitem'
-import Link from 'next/link'
 
   export default function Home() {
   return (
