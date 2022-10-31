b1ee9b9e0b4e656087d39d3e7ae5214f08450c16
JBUkim <91812103@jmail.ac.kr>
2022-10-31 12:30:28 +0900

    11. CreateLoginAPI

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,10 @@ import Head from "next/head";
 import Link from "next/link";
 import { Store } from "../utils/Store";
 import { useContext, useEffect, useState } from "react";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useSession } from "next-auth/react";
+
 
 export default function Layout({ title, children }) {
   const { state } = useContext(Store);
@@ -10,6 +14,8 @@ export default function Layout({ title, children }) {
   useEffect(() => {
     setcartItemsCount(cart.cartItems.reduce((a,c) => a + c.quantity, 0))
   }, [cart.cartItems])
+  const { status, data: session } = useSession()
+
   return (
     <>
       <Head>
@@ -17,6 +23,9 @@ export default function Layout({ title, children }) {
         <meta name="description" content="Nextjs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
+
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex flex-row h-12 items-center px-4 justify-between shadow-md bg-slate-200">
@@ -37,9 +46,11 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link href="/login">
+              {status === 'loading' ? ('Loading') : 
+              session?.user ? ( session.user.name) : 
+              ( <Link href="/login">
                 <a className="p-2">Login</a>
-              </Link>
+              </Link> )}
             </div>
           </nav>
         </header>
--- utils/data.js
+++ utils/data.js
@@ -4,7 +4,7 @@ const data = {
 
   users: [
     {
-      name: 'John',
+      name: 'Admin',
       email: 'admin@example.com',
       password: bcrypt.hashSync('123456'),
       isAdmin: true,
